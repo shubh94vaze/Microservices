0df1c9646cb352e71d7b567d9e86944b14806c40
shubh94vaze <43932115+shubh94vaze@users.noreply.github.com>
2023-11-15 21:55:16 +0530

    CitizenService
    Code for Citizen Service

---
+++ CitizenService/src/main/java/com/codedecode/microservices/CitizenService/Controller/CitizenController.java
@@ -0,0 +1,47 @@
+package com.codedecode.microservices.CitizenService.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codedecode.microservices.CitizenService.Entity.Citizen;
+import com.codedecode.microservices.CitizenService.repositories.CitizenRepo;
+
+//import antlr.collections.List;
+//import com.codedecode.microservices.CitizenService.repositories.CitizenRepo;
+
+@RestController
+@RequestMapping("/citizen")
+public class CitizenController {
+
+	/*
+	 * @Autowired private CitizenRepo repo;
+	 */
+
+	@Autowired
+	private CitizenRepo repo;
+
+	@RequestMapping(path = "/test")
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<String>("Shri Swami Smarth", HttpStatus.OK);
+	}
+
+	@RequestMapping(path = "/id/{id}")
+	public ResponseEntity<java.util.List<Citizen>> getById(@PathVariable Integer id) {
+		List<Citizen> listCitizen = repo.findByVaccinationCenterId(id);
+		return new ResponseEntity<>(listCitizen, HttpStatus.OK);
+	}
+
+	@PostMapping(path = "/add")
+	public ResponseEntity<Citizen> addCitizen(@RequestBody Citizen newCitizen) {
+		Citizen citizen = repo.save(newCitizen);
+		return new ResponseEntity<>(citizen, HttpStatus.OK);
+	}
+}
